d6c87c3c11c1e76907d39993dc1a25731a416511
Grayman1 <>
2021-04-30 04:06:34 +0000

    Rebuild repo - /tests/2_functional-tests.js file. As Replit sucks with corrupted files, constant disconnects and mutliple github conflicts that couldn't be resolved without revert to remote

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,3 +1,4 @@
+// WORKING TESTS
 const chaiHttp = require('chai-http');
 const chai = require('chai');
 let assert = chai.assert;
@@ -6,67 +7,70 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
-  suite('Routing Tests', () => {
-
-    suite('GET /api/convert => conversion object', () => {
-    // FUNCTIONAL TEST #1 -- CONVERT A VALID INPUT
-      test('Convert 10L (valid input)', (done) => {
-        chai.request(server)
-          .get('/api/convert')
-          .query({input: '10L'})
-          .end((err, res) => {
-            assert.equal(res.status, 200);
-            assert.equal(res.body.initNum, 10);
-            assert.equal(res.body.initUnit, 'L');
-            assert.approximately(res.body.returnNum, 2.64172, 0.1);
-            assert.equal(res.body.returnUnit, 'gal');
-            
-          })
-          done();
-      })
-      // FUNCTIONAL TEST #2 -- CONVERT AN INVALID INPUT SUCH AS 32g
-      test('Convert 32g (invalid input unit)', (done) => {
-        chai.request(server)
-          .get('/api/convert')
-          .query({input: '32g'})
-          .end((err, res) => {
-            assert.equal(res.body, 'invalid unit');
-          })
-          done();
-      })
-      // FUNCTIONAL TEST #3 -- CONVERT AN INVALID NUMBER SUCH AS 3/7.2/4KG
-      test('Convert 3/7.2/7kg (invalid number)', (done) => {
-        chai.request(server)
-          .get('/api/convert')
-          .query({input: '3/7.2/7kg'})
-          .end((err, res) => {
-            assert.equal(res.body, 'invalid number');
-          })
-          done();
+<<<<<<< HEAD
+  suite("GET /api/convert => conversion object", () => {
+// Functional Test #1. Convert a valid input such as 10L: GET request to /api/convert.    
+    test("#1 - Convert a valid input", done => {
+      chai.request(server)
+      .get("/api/convert")
+      .query({input: "10L"})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.initNum, 10);
+        assert.equal(res.body.initUnit, "L");
+        assert.approximately(res.body.returnNum, 2.64172, 0.1);
+        assert.equal(res.body.returnUnit, "gal");
+        
       });
+      done();
+    });
 
-      // FUNCTIONAL TEST #4 -- CONVERT AN INVALID NUMBER AND UNIT SUCH AS 3/7.2/4kilomegagram
-      test('Convert 3/7.2/7kilomegagram (invalid number and unit)', (done) => {
-        chai.request(server)
-          .get('/api/convert')
-          .query({input: '3/7.2/7kilomegagram'})
-          .end((err, res) => {
-            assert.equal(res.body, 'invalid number and unit');
-          })
-          done();
-      })
-    // FUNCTIONAL TEST #5 CONVERT WITH NO NUMBER SUCH AS KG
-      test('Convert kg (no number)', (done) => {
-        chai.request(server)
-          .get('/api/convert')
-          .query({input: 'kg'})
-          .end((err, res) => {
-            assert.equal(res.body.initNum, 1);
-            assert.equal(res.body.initUnit, 'kg');
-          })
-          done();
-      })
-    })  
-  })
+// Functional Test #2. Convert an invalid input such as 32g: GET request to /api/convert.    
+    test("#2 - Convert an invalid input", done => {
+      chai.request(server)
+      .get("/api/convert")
+      .query({input: "320mg"})
+      .end((err, res) => {
+        assert.equal(res.body, "invalid unit");        
+      });
+      done();
+    });
+
+// Functional Test #3. Convert an invalid number such as 3/7.2/4kg: GET request to /api/convert.    
+    test("#3 - Convert an invalid number", done => {
+      chai.request(server)
+      .get("/api/convert")
+      .query({input: "3/7.2/4kg"})
+      .end((err, res) => {
+        assert.equal(res.body, "invalid number");        
+      });
+      done();
+    });
+
+// Functional Test #4. Convert an invalid number AND unit such as 3/7.2/4kilomegagram: GET request to /api/convert.    
+    test("#4 - Convert an invalid number AND unit", done => {
+      chai.request(server)
+      .get("/api/convert")
+      .query({input: "3/7.2/4megazilogram"})
+      .end((err, res) => {
+        assert.equal(res.body, "invalid number and unit");        
+      });
+      done();
+    });
+
+// Functional Test #5. Convert with no number such as kg: GET request to /api/convert.    
+    test("#5 - Convert with no number", done => {
+      chai.request(server)
+      .get("/api/convert")
+      .query({input: "kg"})
+      .end((err, res) => {
+        assert.equal(res.body.initNum, 1);
+        assert.equal(res.body.initUnit, "kg");
+      });
+      done();
+    });
+    
+  });
+  
 });
+
